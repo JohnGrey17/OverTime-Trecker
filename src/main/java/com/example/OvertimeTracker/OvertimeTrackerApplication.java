90c2f2eb4923f6f7ba106172b6a64ca3bf7e40c3
Admin <pandastars535@gmail.com>
2025-07-28 19:12:24 +0300

    initial commit

---
+++ src/main/java/com/example/OvertimeTracker/OvertimeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.OvertimeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OvertimeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OvertimeTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OvertimeTracker/OvertimeTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OvertimeTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OvertimeTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
