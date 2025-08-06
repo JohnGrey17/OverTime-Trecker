2cff1e16aa91ff3e32cd257cbbe89081ce6e8e82
Admin <pandastars535@gmail.com>
2025-08-06 17:08:42 +0300

    added salary transaction class made relation between user and salaryTransaction

---
+++ src/main/java/com/example/OvertimeTracker/model/SalaryTransaction.java
@@ -0,0 +1,30 @@
+package com.example.OvertimeTracker.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "salary_transactions")
+@Data
+public class SalaryTransaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount; // сума виплати
+
+    private LocalDate startDate; // з якого дня розрахунок
+    private LocalDate endDate;   // по який день включно
+
+    private LocalDate transactionDate; // дата коли виплатили
+
+    private String calculatedBy; // хто натискав "порахувати ЗП"
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+}
