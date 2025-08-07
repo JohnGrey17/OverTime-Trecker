5878470af20ad3c4000a650122d67e19cfd67239
Admin <pandastars535@gmail.com>
2025-08-07 18:23:15 +0300

    added mapper and mapper configuration

---
+++ src/main/java/com/example/OvertimeTracker/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.OvertimeTracker.mapper;
+
+import com.example.OvertimeTracker.config.MapperConfig;
+import com.example.OvertimeTracker.dto.UserRegistrationRequestDto;
+import com.example.OvertimeTracker.model.User;
+import org.mapstruct.Mapper;
+
+
+@Mapper(config = MapperConfig.class)
+public interface UserMapper {
+    User toModel(UserRegistrationRequestDto requestDto);
+
+}
