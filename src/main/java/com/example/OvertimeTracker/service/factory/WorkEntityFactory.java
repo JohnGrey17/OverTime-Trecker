daf1aaa023c47efa42560541f61b6cae1ddcf6af
Admin <pandastars535@gmail.com>
2025-08-07 18:23:53 +0300

    added Factory to provide creation of new object

---
+++ src/main/java/com/example/OvertimeTracker/service/factory/WorkEntityFactory.java
@@ -0,0 +1,54 @@
+package com.example.OvertimeTracker.factory;
+
+import com.example.OvertimeTracker.dto.MissingWorkDateRequestDto;
+import com.example.OvertimeTracker.dto.OvertimeRequestDto;
+import com.example.OvertimeTracker.exceptions.types.UserException;
+import com.example.OvertimeTracker.model.MissingWorkDays;
+import com.example.OvertimeTracker.model.OverTimeWork;
+import com.example.OvertimeTracker.model.User;
+import com.example.OvertimeTracker.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+@Component
+@RequiredArgsConstructor
+public class WorkEntityFactory {
+
+    private final UserRepository userRepository;
+
+    public MissingWorkDays createMissingWorkDay(MissingWorkDateRequestDto dto, Long userId) {
+        User user = getUser(userId);
+        MissingWorkDays entity = new MissingWorkDays();
+        entity.setDate(dto.getDate());
+        entity.setReason(dto.getReason());
+        entity.setMissingHours(dto.getMissingHours());
+        entity.setUser(user);
+        return entity;
+    }
+
+    public OverTimeWork createOvertimeWork(OvertimeRequestDto dto, Long userId) {
+        User user = getUser(userId);
+        OverTimeWork entity = new OverTimeWork();
+        entity.setOverTimeDateRegistration(dto.getOverTimeDateRegistration());
+        entity.setDescription(dto.getDescription());
+        entity.setOvertime_hours(dto.getOvertime_hours());
+        entity.setMultiplier(calculateMultiplier(dto.getOverTimeDateRegistration()));
+        entity.setUser(user);
+        return entity;
+    }
+
+    private BigDecimal calculateMultiplier(LocalDate date) {
+        DayOfWeek day = date.getDayOfWeek();
+        return (day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY) ?
+                BigDecimal.valueOf(1.5) : BigDecimal.ONE;
+    }
+
+    private User getUser(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserException("User with that id does not exist"));
+    }
+}
