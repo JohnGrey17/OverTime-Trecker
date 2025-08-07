28fea953f9f0b862d85136824bc768214b961b22
Admin <pandastars535@gmail.com>
2025-08-07 18:17:32 +0300

    added registration / authorization system
    added all dependencies for JWT and spring security
    
    implemented user details interface
    added all DTO for registration
    added role for user

--- src/main/java/com/example/OvertimeTracker/model/User.java
+++ src/main/java/com/example/OvertimeTracker/model/User.java
@@ -1,24 +1,43 @@
     package com.example.OvertimeTracker.model;
 
+    import com.example.OvertimeTracker.model.roles.Role;
     import jakarta.persistence.*;
     import lombok.Data;
+    import org.springframework.security.core.GrantedAuthority;
+    import org.springframework.security.core.userdetails.UserDetails;
 
     import java.math.BigDecimal;
+    import java.util.Collection;
     import java.util.HashSet;
     import java.util.Set;
 
     @Entity
     @Table(name = "users")
     @Data
-    public class User {
+    public class User implements UserDetails {
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         private Long id;
         private String firstName;
         private String lastName;
-        private BigDecimal Salary;
+        private BigDecimal salary;
         private String phoneNumber;
+        private String departmentName;
+
+        @ManyToMany(fetch = FetchType.EAGER)
+        @JoinTable(
+                name = "users_roles",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id")
+        )
+        private Set<Role> roles;
+
+        @Column(nullable = false,unique = true)
+        private String email;
+
+        @Column(nullable = false)
+        private String password;
 
         @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
         private Set<OverTimeWork> overTimeWorks = new HashSet<>();
@@ -28,5 +47,40 @@
 
         @OneToMany(mappedBy = "user",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
         private Set<MissingWorkDays> missingWorkDays = new HashSet<>();
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return roles;
+        }
+
+        @Override
+        public String getUsername() {
+            return email;
+        }
+
+        @Override
+        public String getPassword() {
+            return password;
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
     }
 
