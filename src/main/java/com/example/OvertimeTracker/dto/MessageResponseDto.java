5c84b8ea8beaf3cad71cfc889a30ab1da7159712
Admin <pandastars535@gmail.com>
2025-08-07 18:31:16 +0300

    added MessageResponseDto and UserResponseDto

---
+++ src/main/java/com/example/OvertimeTracker/dto/MessageResponseDto.java
@@ -0,0 +1,10 @@
+package com.example.OvertimeTracker.dto;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+public class MessageResponseDto {
+    private final String message;
+}
---
+++ src/main/java/com/example/OvertimeTracker/dto/UserResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.OvertimeTracker.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class UserResponseDto {
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private List<String> userRoles;
+}
