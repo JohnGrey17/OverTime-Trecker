1fc9c2f6f7f5309d5bf8b5c8125d6654b5ed2ddc
NewJohnGrey17 <salounlimited32@gmail.com>
2025-09-10 16:12:32 +0300

    Added Corps configuration

---
+++ src/main/java/com/example/OvertimeTracker/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.OvertimeTracker.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:63342")  // 👈 твій фронтенд
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
