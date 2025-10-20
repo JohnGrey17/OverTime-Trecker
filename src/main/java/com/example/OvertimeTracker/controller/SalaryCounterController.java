9764781b91e3ad677820541dc3169c39babd7dec
Serj <pandastars535@gmail.com>
2025-10-20 22:21:54 +0300

    feat(salaryController): delete salary controller

--- src/main/java/com/example/OvertimeTracker/controller/SalaryCounterController.java
+++ src/main/java/com/example/OvertimeTracker/controller/SalaryCounterController.java
@@ -1,22 +1,22 @@
-package com.example.OvertimeTracker.controller;
-
-import com.example.OvertimeTracker.service.salaryCounter.aggregator.SalaryAggregatorService;
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.web.bind.annotation.*;
-
-
-//@RestController
-@RequestMapping("/count")
-@RequiredArgsConstructor
-public class SalaryCounterController {
-
-
-    private final SalaryAggregatorService salaryCounterService;
-
-//    @GetMapping("/get")
-//    @PreAuthorize("hasRole('USER')")
-//    public BigDecimal addOverTime(@RequestBody SalaryCounterRequestDto requestDto, @AuthenticationPrincipal User user) {
-//        return salaryCounterService.calculateSalaryForUser(user.getId(), requestDto.getDate());
-//    }
-}
+//package com.example.OvertimeTracker.controller;
+//
+//import com.example.OvertimeTracker.service.salaryCounter.aggregator.SalaryAggregatorService;
+//import lombok.RequiredArgsConstructor;
+//
+//import org.springframework.web.bind.annotation.*;
+//
+//
+////@RestController
+//@RequestMapping("/count")
+//@RequiredArgsConstructor
+//public class SalaryCounterController {
+//
+//
+//    private final SalaryAggregatorService salaryCounterService;
+//
+////    @GetMapping("/get")
+////    @PreAuthorize("hasRole('USER')")
+////    public BigDecimal addOverTime(@RequestBody SalaryCounterRequestDto requestDto, @AuthenticationPrincipal User user) {
+////        return salaryCounterService.calculateSalaryForUser(user.getId(), requestDto.getDate());
+////    }
+//}
